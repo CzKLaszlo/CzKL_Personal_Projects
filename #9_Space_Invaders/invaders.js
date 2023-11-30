cba1761fa19a326e76aacbdb335e36a1698a9c4e
Cziprián-Kovács László <cziprian.k.laszlo@gmail.com>
2023-11-30 13:51:04 +0200

    Add files via upload

--- #9_Space_Invaders/invaders.js
+++ #9_Space_Invaders/invaders.js
@@ -85,9 +85,53 @@ class Projectile {
   }
 }
 
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./Images/Alien.png";
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2,
+      };
+    };
+  }
+
+  draw() {
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    if (this.image)
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+  }
+  update() {
+    if (this.image) {
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+}
+
 const player = new Player();
 const projectiles = [];
-
+const invader = new Invader();
 const keys = {
   a: {
     pressed: false,
@@ -102,6 +146,7 @@ const keys = {
 
 function animate() {
   requestAnimationFrame(animate);
+  invader.update();
   player.update();
 
   projectiles.forEach((projectile, index) => {
