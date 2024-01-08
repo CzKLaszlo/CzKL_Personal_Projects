d7a640569b1c670a9ba06b287e01a5ee22711742
Cziprián-Kovács László <cziprian.k.laszlo@gmail.com>
2024-01-08 19:43:08 +0200

    Screenshot_button
    This simple project uses a screenshot mechanic in form of a button utilizing the free repository https://html2canvas.hertzen.com/dist/html2canvas.min.js.

---
+++ #12.1_Screenshot_button/index.js
@@ -0,0 +1,21 @@
+document.getElementById("screenshotBtn").addEventListener("click", function () {
+  //Hide scrrenshot buttton from screenshot
+  this.classList.add("hidden");
+
+  html2canvas(document.documentElement, {
+    scale: window.devicePixelRatio,
+    logging: true,
+    useCORS: true,
+    windowHeight: window.innerHeight,
+    windowWidth: window.innerWidth,
+  }).then((canvas) => {
+    var image = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    var link = document.createElement("a");
+    link.download = "screenshot.png";
+    link.href = image;
+    link.click();
+    this.classList.remove("hidden");
+  });
+});
